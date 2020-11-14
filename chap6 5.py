a26ec4ceaea1c2418c76a8eecf7d89c3e8170beb
disha shetty <50168017+dishashetty5@users.noreply.github.com>
2020-11-14 13:18:42 +0530

    Add files via upload

---
+++ chap6 5.py
@@ -0,0 +1,11 @@
+"""Take the following Python code that stores a string:
+str = 'X-DSPAM-Confidence:0.8475'
+Use find and string slicing to extract the portion of the string after the
+colon character and then use the float function to convert the extracted
+string into a floating point number."""
+str = 'X-DSPAM-Confidence:0.8475'
+m=str.find(":")
+n=len(str)
+slicing=str[m+1:n]
+num=float(slicing)
+print(num)
